61ebf8bf340577b867cc501cc091e3c030d01523
The Winter Shadow <elijah.j.winter@outlook.com>
2025-09-19 23:50:55 +0000

    Update Hatch environment commands and add a new line in pyproject.toml

--- execution/docs/source/conf.py
+++ execution/docs/source/conf.py
@@ -1,13 +1,13 @@
 # Configuration file for the Sphinx documentation builder.
 
-project = 'Owl-Watch'
-copyright = '2025, The Winter Shadow'
-author = 'The Winter Shadow'
-release = '0.1.0'
+project = "Owl-Watch"
+copyright = "2025, The Winter Shadow"
+author = "The Winter Shadow"
+release = "0.1.0"
 
 extensions = []
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
